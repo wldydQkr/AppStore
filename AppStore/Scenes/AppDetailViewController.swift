c6c9d22e23390eb37947f76d9d7f7817c9cb0d62
wlQkr <wlqkr1010@gmail.com>
2022-04-26 23:14:34 +0900

    Share Sheet

--- AppStore/Scenes/AppDetailViewController.swift
+++ AppStore/Scenes/AppDetailViewController.swift
@@ -51,6 +51,7 @@ final class AppDetailViewController: UIViewController {
         let button = UIButton()
         button.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
         button.tintColor = .systemBlue
+        button.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
         
         return button
     }()
@@ -120,6 +121,12 @@ private extension AppDetailViewController {
             $0.trailing.equalTo(titleLabel.snp.trailing)
             $0.width.equalTo(32.0)
         }
+    }
+    
+    @objc func didTapShareButton() {
+        let activityItems: [Any] = [today.title]
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
     }
     
 }
